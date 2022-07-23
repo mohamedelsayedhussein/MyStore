d6050028e13946441e741dff2573bd4f929ccfdd
mohamedelsayedhussein <mohamedelsayedhussein0@gmail.com>
2022-07-23 02:07:54 +0200

    Add Form Group

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
@@ -11,6 +12,7 @@ import { ConfirmationComponent } from './components/confirmation/confirmation.co
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { productDetailsComponent } from './components/product-details/product-details.component';
+import { InputComponent } from './layout/input/input.component';
 
 @NgModule({
   declarations: [
@@ -21,12 +23,14 @@ import { productDetailsComponent } from './components/product-details/product-de
     ProductItemComponent,
     productDetailsComponent,
     ProductListComponent,
+    InputComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -9,11 +10,30 @@ export class CartComponent implements OnInit {
   cartProducts: any[] = [];
   total: number = 0;
   success: boolean = false;
+  form!: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
     this.getCartProducts();
+
+    this.form = new FormGroup({
+      fullName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      address: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      creditNumber: new FormControl('', [Validators.required, Validators.pattern("^([0-9]{4}[\s-]?){3}([0-9]{4})$")]),
+    })
+  }
+
+  get fullName() {
+    return this.form.get('fullName');
+  }
+
+  get address() {
+    return this.form.get('address');
+  }
+
+  get creditNumber() {
+    return this.form.get('creditNumber');
   }
 
   getCartProducts() {
@@ -39,5 +59,9 @@ export class CartComponent implements OnInit {
     this.cartProducts.splice(index, 1);
     this.getCartTotal();
     localStorage.setItem("cart", JSON.stringify(this.cartProducts));
+  }
+
+  onSubmit() {
+    this.success = true;
   }
 }
